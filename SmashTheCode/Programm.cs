970a7f134f0dd433a3a6e87ec54b20d6d6073a57
Zygelon <zygelon@gmail.com>
2017-09-13 21:12:49 +0300

    Input v4.0. Start developing simulation

--- SmashTheCode/Programm.cs
+++ SmashTheCode/Programm.cs
@@ -21,11 +21,12 @@ struct Color
     }
 }
 
+enum Rotation { Right,Up,Left,Down};
+
 class Player
 {
-    public const int ROWS = 6;
-    public const int COLUMNS = 12;
-    private string[] grid;
+    public const int ROWS = 6, COLUMNS = 12;
+    private char[,] grid;
     private bool isKnownNextBalls;
     public Color[] NextBalls { get; private set; }
     public int Score { get; private set; }
@@ -33,12 +34,11 @@ class Player
 
     public Player(Color[] NextBalls=null){
         HeightBalls = new int[ROWS];
-        for (int i = 0; i < ROWS; ++i)
-            HeightBalls[i] = -1;
         Score = 0;
-        grid = new string[COLUMNS];
+        grid = new char[COLUMNS,ROWS];
         if (NextBalls == null)
         {
+            Console.Error.WriteLine("22222");
             isKnownNextBalls = false;
             this.NextBalls = new Color[8];
         }
@@ -52,7 +52,7 @@ class Player
     public void UpdateInput()
     {
         for (int i = 0; i < ROWS; ++i)
-            HeightBalls[i] = -1;
+            HeightBalls[i] = 0;
         if (!isKnownNextBalls)
             for (int i = 0; i < 8; i++)
             {
@@ -62,51 +62,76 @@ class Player
         Score = int.Parse(Console.ReadLine());
         for (int i = 0; i < COLUMNS; ++i)
         {
-            grid[i] = Console.ReadLine();
             for (int j = 0; j < ROWS; ++j)
-                if (HeightBalls[j] == -1 && grid[i][j] != '.') HeightBalls[j] = COLUMNS - 1 - i;
+            {
+                grid[i, j] = (char)Console.Read();
+                if (HeightBalls[j] == 0 && grid[i, j] != '.') HeightBalls[j] = COLUMNS - i;
+            }
+            Console.Read();//ignore end of line
         }
     }
     public void ShowInputInformation()
     {
         Console.Error.Write("Balls:");
         foreach (var t in NextBalls)
-            Console.Error.Write(t.a + t.b + " ");
+            Console.Error.Write(t.a +""+t.b + " ");
         Console.Error.WriteLine();
 
         Console.Error.WriteLine("Score="+Score);
 
         Console.Error.Write("HeightBalls:");
-        foreach (var t in HeightBalls)
-            Console.Error.Write(t + " ");
+        foreach (var el in HeightBalls)
+            Console.Error.Write(el + " ");
         Console.Error.WriteLine();
 
         for (int i = 0; i < COLUMNS; ++i)
-            Console.Error.WriteLine(grid[i]);
-        Console.Error.WriteLine();
+        {
+            for (int j = 0; j < ROWS; ++j)
+                Console.Error.Write(grid[i, j] + " ");
+            Console.Error.WriteLine();
+        }
     }
 }
-class BaseStrategy
-{
-    //virtual protected int Score(int b,int cb,int cp,int gb)//b - ощичено блоков за этот ход, cp=chain power
+    //b - ощичено блоков за этот ход, cp=chain power
     //CP is the chain power, starting at 0 for the first step. It is worth 8 for the second step and for each following step it is worth twice as much as the previous step.
         //CB is the Color Bonus, depending on how many different color blocks were cleared in the step. It is worth 
         //GB is the Group Bonus, depending on how many blocks are destroyed per group. When several groups have been cleared, the bonuses of all groups are summed. It is worth 
         //The value of(CP + CB + GB) is limited to between 1 and 999 inclusive.
+class Simulate
+{
+    private const int ROWS = 6, COLUMNS = 12;
+    char[,] currentGrid;
+    int B, CP, CB, GB;
+    Simulate(char[,] currentGrid)
+    {
+        this.currentGrid = currentGrid;
+        B = 0; CP = 0; CB = 0; GB = 0;
+    }
+    void SimulateMove(int x,Rotation rot)
+    {
+        if(x==0 && rot==Rotation.Left || x==ROWS-1 && rot==Rotation.Right)
+        {
+            Console.Error.WriteLine("A ball isn't in the Grid");
+            return;
+        }
+    }
+    void FindAndDestroy(int x,int y)
+    {
 
-    
+    }
 }
+    
 
 class Program {
-    static void Main(string[] args)
+    static void Main()
     {
         Player me = new Player();
-        Player enemy = new Player(me.NextBalls);
         while (true)
         {
             me.UpdateInput();
-            enemy.UpdateInput();
             me.ShowInputInformation();
+        Player enemy = new Player(me.NextBalls);
+            enemy.UpdateInput();
             enemy.ShowInputInformation();
             Console.WriteLine("0 1");
         }
